674b4ac7f69c5abb4ad6f0262f45fac419627d5e
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-02 00:54:25 +0400

    Create Exercise2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -0,0 +1,4 @@
+#Find a quote from a famous person you admire. Print the quote and the name of its author. 
+author = "Nelson Mandela"
+quote = "I never lose. I either win or learn."
+print(f'{author} once said, "{quote}"')
