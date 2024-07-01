39aa6246d1bba2cc5f6d055daa31b1a54f6cf0a2
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-02 00:04:00 +0400

    Create Exercise4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,9 @@
+#Write three strings in different variables and print the output as one string.
+Greeting="hey what's up,"
+Relative="Cousin!"
+Question="Let's go out tonight?"
+combined=Greeting+Relative+Question
+print(combined)
+
+
+
