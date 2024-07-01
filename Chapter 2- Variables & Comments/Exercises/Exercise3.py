f5d4b61ae4400a2f060d8e14dade5b98dba7498c
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-02 00:54:36 +0400

    Create Exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -0,0 +1,13 @@
+#Tidy up the code to make it easier to understand
+Person_name = "\t   Nabila\n  "
+person_name = "\t   Fazila\n  "
+persons_name = "\t   Sarah\n  "
+Person_Name = "\t   Kainat\n  "
+print("Name with whitespace:", Person_name) 
+print("Name with left stripping:", person_name.lstrip())
+print("Name with right stripping:", persons_name.rstrip())
+print("Name with both stripping:", Person_Name.strip())
+
+
+
+
