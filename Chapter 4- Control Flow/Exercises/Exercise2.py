cb03a57d00391004ed39cc64cf1fab4cf2c2fb69
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-02 17:45:59 +0400

    Create Exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,16 @@
+#Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+alien_color = "Yellow"
+
+if alien_color == "Yellow":
+    print("Congrats on earning 5 points for shooting the yellow alien.")
+else:
+    print("block")
+
+# Write one version of this program that runs the if block and another that runs the else block.
+alien_color = "Red"  
+if alien_color == "green":
+    print("Congrats!!You have earned yourself 5 points for shooting the green alien.")
+else:
+    print("block")
+
+
