272f29e7d73728963cba01d16675efb5f3632387
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-02 17:38:56 +0400

    Create Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,12 @@
+#If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner.
+Guests = ("Romesa", "Abrish", "Meerub")
+for Guest in Guests:
+    invitation = f"Dear {Guest},\n\nWe are pleased to invite you to that you have been invited to the upcoming dinner party held at our residence. We would be pleased if you were to come over and enjoy this relaxing eveing with us. Please inform us about if you are interested in attending\n\nyour_truly, Anabia Wasti"
+    print(invitation)
+
+
+
+
+
+
+
