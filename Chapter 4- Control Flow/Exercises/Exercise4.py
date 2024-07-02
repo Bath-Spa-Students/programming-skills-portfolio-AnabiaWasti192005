258e6c6781a1bd73ebf0537e4cb84097b31b00b0
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-02 17:46:11 +0400

    Create Exercise4.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise4.py
@@ -0,0 +1,17 @@
+#Write an if-elif-else chain that determines a person’s stage of life.
+age = 100
+
+if age < 2:
+    print("The person is a baby.")
+elif 2 <= age < 4:
+    print("The person is a toddler.")
+elif 4 <= age < 13:
+    print("The person is a kid.")
+elif 13 <= age < 20:
+    print("The person is a teenager.")
+elif 20 <= age < 65:
+    print("The person is an adult.")
+else:
+    print("The person is an elder.")
+
+
