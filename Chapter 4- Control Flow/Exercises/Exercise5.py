2570232bcf0552684278449e8a4652a3af993be9
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-02 17:46:17 +0400

    Create Exercise5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -0,0 +1,14 @@
+#Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+favorite_fruits = ("Orange", "Apple", "Blueberry", "Pears")
+if "Pears" in favorite_fruits:
+    print("You sure enjoy pears!")
+if "Blueberry" in favorite_fruits:
+    print("You sure enjoy Blueberries!")
+if "Cherry" in favorite_fruits:
+    print("You sure enjoy Cherries!")
+if "Orange" in favorite_fruits:
+    print("You sure enjoy Orange!")
+if "Apple" in favorite_fruits:
+    print("You sure enjoy Apple!")
+if "Grapes" in favorite_fruits:
+    print("You sure enjoy grapes!")
