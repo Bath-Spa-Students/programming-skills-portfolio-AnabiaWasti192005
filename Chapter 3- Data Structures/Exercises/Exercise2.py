e871e319caa47e33c5509f75cdee8f3811c6fea4
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-02 17:38:47 +0400

    Create Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,10 @@
+#Start with the list you used in Exercise 1, but instead of just printing each person’s name, print a message to them.
+Names = ("Khuloud","Farah","Minal","Sameen","Hafsa", "Zainab")
+Message = "Hi, {}! Hope you're doing well."
+for name in Names:
+    print(Message.format(name))
+
+
+
+
+
