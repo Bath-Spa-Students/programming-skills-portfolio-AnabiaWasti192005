e1bb900144e3b8757232b03567219fd0fcbe1037
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-02 17:38:51 +0400

    Create Exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,13 @@
+#Think of your favorite mode of transportation, such as a motorcycle or a car
+modes_of_transportation = ("Toyota Car","Cruise","Mustang","Plane", "Bus")
+for mode in modes_of_transportation : 
+    if mode == "Toyota Car":
+        print("I'm interested in buying a Toyota car")
+    if mode ==  "Cruise":
+        print("Riding on a cruise is what I wish for")
+    if mode ==  "Mustang":
+        print("I'm in love with that black mustang")
+    if mode == "Plane":
+       print("Travelling by plane is so relaxing")
+    if mode == "Bus":
+       print("Going by bus can be fun")
