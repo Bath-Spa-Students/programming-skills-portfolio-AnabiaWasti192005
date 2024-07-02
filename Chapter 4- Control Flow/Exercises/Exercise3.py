a52c6d89023c7829ba0ab27d325bb9cf1943f73b
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-02 17:46:06 +0400

    Create Exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise3.py
@@ -0,0 +1,33 @@
+#Turn your if-else chain from Exercise 5-4 into an if-elifelse chain.
+alien_color = "green"
+
+if alien_color == "green":
+    print("The player earned 5 points.")
+elif alien_color == "yellow":
+    print("The player earned 10 points.")
+elif alien_color == "red":
+    print("The player earned 15 points.")
+
+
+#If the alien is yellow, display a message that  10 points have been earned.
+alien_color = "red"
+
+if alien_color == "green":
+    print("The player earned 10 points.")
+elif alien_color == "yellow":
+    print("The player earned 10 points.")
+elif alien_color == "red":
+    print("The player earned 15 points.")
+
+#If the alien is red, display a message that  15 points have been earned.
+alien_color = "yellow"
+
+if alien_color == "green":
+    print("The player earned 5 points.")
+elif alien_color == "yellow":
+    print("The player earned 10 points.")
+elif alien_color == "red":
+    print("The player earned 15 points.")
+
+
+
