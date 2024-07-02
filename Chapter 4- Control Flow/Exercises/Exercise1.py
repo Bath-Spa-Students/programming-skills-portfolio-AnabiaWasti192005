a2b27bb0547abae0a697136fa71da8d8c31713b8
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-02 17:39:19 +0400

    Create Exercise1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,9 @@
+#Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+alien_color = 'Red'
+if alien_color == 'Red':
+    # Print a message that the player earned 5 points
+    print("Congrats on achieving 5 points!!")
+
+
+
+
