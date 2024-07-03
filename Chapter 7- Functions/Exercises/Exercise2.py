9b69d93714955cea5d74da00b6acffa484c4694a
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-03 17:57:07 +0400

    Create Exercise2.py

---
+++ Chapter 7- Functions/Exercises/Exercise2.py
@@ -0,0 +1,13 @@
+#Write a function called favorite_book() that accepts one parameter, title.
+def Favorite_book(title):
+    print(f"The book I enjoy reading alot is {title}.")
+
+Favorite_book("Dork Dairies")
+
+
+
+
+
+
+
+
