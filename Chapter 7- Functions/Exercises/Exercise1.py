40668ebbacf913fcd1751bbb82b3ae49587edd2e
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-03 17:57:01 +0400

    Create Exercise1.py

---
+++ Chapter 7- Functions/Exercises/Exercise1.py
@@ -0,0 +1,9 @@
+#Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter.
+def display_message(Message):
+    print(Message)
+
+# Define the printed message given below
+My_Message = "From chapter 7, we are discovering how can functions work."
+
+# Call this function with the given message
+display_message(My_Message)
