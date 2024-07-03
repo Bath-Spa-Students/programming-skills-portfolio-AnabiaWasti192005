54e2cd7e1250ef55c72c17fa60eb911c8f43fd9f
AnabiaWasti192005 <159002229+AnabiaWasti192005@users.noreply.github.com>
2024-07-03 17:57:23 +0400

    Create Exercise5.py

---
+++ Chapter 7- Functions/Exercises/Exercise5.py
@@ -0,0 +1,16 @@
+#Write a function called describe_city() that accepts the name of a city and its country.
+def City_1(city, country="default country"):
+    """Prints a sentence describing the city and its country."""
+    print(f"{city} is in {country}.")
+
+# Call the function for three different cities
+City_1("Baku", "Azerbaijan")
+City_1("Tokyo", "Japan")
+City_1("Singapore")
+
+
+
+
+
+
+
